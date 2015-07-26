36666e77507e7d1cd390c4ee656e95652da1d5c8
Alexander Neumann <alexander@bumpern.de>
2015-07-26 22:04:23 +0200

    More work on repacker

--- checker/repacker.go
+++ checker/repacker.go
@@ -1,6 +1,8 @@
 package checker
 
 import (
+	"fmt"
+
 	"github.com/restic/restic/backend"
 	"github.com/restic/restic/repository"
 )
@@ -18,25 +20,9 @@ func (r *Repacker) Repack() error {
 	return nil
 }
 
-// uniqIDs returns list without any duplicate IDs.
-func uniqIDs(list []backend.ID) []backend.ID {
-	known := make(map[mapID]struct{})
-	for pos := 0; pos < len(list); pos++ {
-		id := list[pos]
-		if _, ok := known[id2map(id)]; ok {
-			list = append(list[:pos], list[pos+1:]...)
-			continue
-		}
-
-		known[id2map(id)] = struct{}{}
-	}
-
-	return list
-}
-
-// FindPacksforBlobs returns the list of packs that contain the blobs.
-func FindPacksforBlobs(repo *repository.Repository, blobs []backend.ID) ([]backend.ID, error) {
-	var packs []backend.ID
+// FindPacksforBlobs returns the set of packs that contain the blobs.
+func FindPacksforBlobs(repo *repository.Repository, blobs []backend.ID) (backend.IDSet, error) {
+	packs := backend.NewIDSet()
 	idx := repo.Index()
 	for _, id := range blobs {
 		packID, _, _, _, err := idx.Lookup(id)
@@ -44,8 +30,12 @@ func FindPacksforBlobs(repo *repository.Repository, blobs []backend.ID) ([]backe
 			return nil, err
 		}
 
-		packs = append(packs, packID)
+		if packID == nil {
+			return nil, fmt.Errorf("blob id %v not found in any index", id)
+		}
+
+		packs.Insert(*packID)
 	}
 
-	return uniqIDs(packs), nil
+	return packs, nil
 }
